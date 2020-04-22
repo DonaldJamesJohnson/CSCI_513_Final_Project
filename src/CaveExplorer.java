b62fa75a207580593dc0dfbf24520b74f58ac52a
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-04-22 00:25:33 -0500

    Implemented shooting function

--- src/CaveExplorer.java
+++ src/CaveExplorer.java
@@ -1,13 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
+import java.util.stream.Collectors;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -29,19 +26,16 @@ public class CaveExplorer extends Application {
     Player player = new Player(20, 20, baseRect);
     
     Enemy enemy = new Enemy(caveMap.getTileSize());
-    Enemy enemy2 = new Enemy(caveMap.getTileSize());
+    List<Bullet> bullets = new ArrayList<Bullet>();
     
-
-    @Override
     public void start(Stage primaryStage) {
     	// Create pane 
         // Create player 
         pane.getChildren().add(player.playerRect);
 		player.addObserver(enemy);
-		player.addObserver(enemy2);
-		player.Timer(pane);
-		enemy.Timer(pane);
-		enemy2.Timer(pane);
+		//player.addObserver(enemy2);
+		//enemy.Timer(pane);
+		//enemy2.Timer(pane);
         // Create scene
         Scene scene = new Scene(new BorderPane(pane), 800, 800);
         // Set clip for scene
@@ -59,34 +53,151 @@ public class CaveExplorer extends Application {
         pane.setClip(clip);
         pane.translateXProperty().bind(clip.xProperty().multiply(-1));
         pane.translateYProperty().bind(clip.yProperty().multiply(-1));
-		enemy.addToPane(pane);
-		enemy2.addToPane(pane);
+		pane.getChildren().add(enemy.enemySprite.circle);
+		//enemy2.addToPane(pane);
+		
+		AnimationTimer timer = new AnimationTimer() {
+            private long lastUpdate = 0 ;
+            @Override
+            public void handle(long now) {
+                long elapsedNanos = now - lastUpdate ;
+                if (lastUpdate < 0) {
+                    lastUpdate = now ;
+                    return ;
+                }
+                double elapsedSeconds = elapsedNanos / 1_000_000_000.0 ;
+                double deltaX = 0 ;
+                double deltaY = 0 ;
+                double shootDX = 0;
+                double shootDY = 0;
+                if (player.right) deltaX += player.speed ;
+                if (player.left) deltaX -= player.speed ;
+                if (player.down) deltaY += player.speed ;
+                if (player.up) deltaY -= player.speed ;
+                player.playerRect.setX(clampRange(player.playerRect.getX() + deltaX * elapsedSeconds, 0, pane.getWidth() - player.playerRect.getWidth()));
+                player.playerRect.setY(clampRange(player.playerRect.getY() + deltaY * elapsedSeconds, 0, pane.getHeight() - player.playerRect.getHeight()));
+    	    	
+                enemy.enemySprite.setX(clampRange(enemy.enemySprite.circle.getCenterX() + (enemy.xMove * elapsedSeconds), 0, pane.getWidth() - enemy.enemySprite.circle.getRadius()));
+                enemy.enemySprite.setY(clampRange(enemy.enemySprite.circle.getCenterY() + (enemy.yMove * elapsedSeconds), 0, pane.getHeight() - enemy.enemySprite.circle.getRadius()));
+                enemy.move();
+                
+                if (!bullets.isEmpty())
+                {
+                    for(Bullet b : bullets) {
+                    	if (b.direction == "up") b.moveUp();
+                    	if (b.direction == "down") b.moveDown();
+                    	if (b.direction == "left") b.moveLeft();
+                    	if (b.direction == "right") b.moveRight();
+                    	if (b.getBoundsInParent().intersects(enemy.enemySprite.circle.getBoundsInParent())) pane.getChildren().remove(enemy.enemySprite.circle);
+                    }
+                }
+                lastUpdate = now ;
+            }
+        };
         
         // Player movement
-        scene.setOnKeyPressed(e -> player.processKey(e.getCode(), true));
-        scene.setOnKeyReleased(e -> player.processKey(e.getCode(), false));
+        scene.setOnKeyPressed(e -> processKey(e.getCode(), true));
+        scene.setOnKeyReleased(e -> processKey(e.getCode(), false));
 
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        player.playerTimer.start();
-        enemy.enemyTimer.start();
-        enemy2.enemyTimer.start();
+        
+        timer.start();
+       // player.playerTimer.start();
+       // enemy.enemyTimer.start();
+       // enemy2.enemyTimer.start();
     }
+
+
+    
+    public void processKey(KeyCode code, boolean on) {
+        switch (code) {
+        case A:
+        	player.move();
+            player.left = on ;
+            break ;
+        case D: 
+    		player.move();
+    		player.right = on ;
+            break ;
+        case W:
+    		player.move();
+    		player.up = on ;
+            break ;
+        case S:
+    		player.move();
+    		player.down = on ;
+            break ;
+
+        case UP: 
+        	shoot("up");
+        	break;
+        case LEFT:
+        	shoot("left");
+        	break;
+        case RIGHT: 
+        	shoot("right");
+        	break;
+        case DOWN:
+        	shoot("down");
+        	break;
+        	
+        default:
+            break ;
+        }
+    }
+
 
     private double clampRange(double value, double min, double max) {
         if (value < min) return min ;
         if (value > max) return max ;
         return value ;
     }
-
-    private void shoot(Player who)
-    {
-    	Rectangle bullet = new Rectangle(who.playerRect.getTranslateX() + 20, who.playerRect.getTranslateY(), 5, 20);
-    	bullet.setFill(Color.BLACK);
-    	pane.getChildren().add(bullet);
-    }
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    
+    private void shoot(String direction)
+    {
+    	Bullet b = new Bullet(player.getPlayerLocationX() + 10, player.getPlayerLocationY() + 10, 5, 5, direction, Color.BLACK);
+    	bullets.add(b);
+    	pane.getChildren().add(b);
+    }
+    
+    
+    
+    private static class Bullet extends Rectangle {
+    	final String direction;
+    	boolean dead = false;
+    	
+    	Bullet(double x, double y, double w, double h, String dir, Color color)
+    	{
+    		super(w, h, color);
+    		this.direction = dir;
+    		setTranslateX(x);
+    		setTranslateY(y);    	
+    	}
+    	
+		void moveLeft()
+		{
+			setTranslateX(getTranslateX() - 1);
+		}
+		
+		void moveRight()
+		{
+			setTranslateX(getTranslateX() + 1);
+		}
+		
+		void moveUp()
+		{
+			setTranslateY(getTranslateY() - 1);
+		}
+		
+		void moveDown()
+		{
+			setTranslateY(getTranslateY() + 1);
+		}
+		
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -5,23 +5,25 @@
  * Purpose: The Player class stores and changes the player's location when a prompted by a key event triggered in CaveExplorer.
  * 			Instances of this class are observed by instances of Enemy. 
  */
-import java.util.Observable;
 
-import javafx.animation.AnimationTimer;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
+import java.util.Observable;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Player extends Observable
 {
     Rectangle playerRect = new Rectangle();
-    AnimationTimer playerTimer;
     int speed = 500;
-    private boolean up ;
-    private boolean down ;
-    private boolean left ;
-    private boolean right ;
+    int shootSpeed = 100;
+    public boolean up ;
+    public boolean down ;
+    public boolean left ;
+    public boolean right ;
+    public boolean shoot;
+    public boolean shootUp;
+    public boolean shootDown;
+    public boolean shootLeft;
+    public boolean shootRight;
 	
 	public Player(int X, int Y, Rectangle r) 
 	{
@@ -34,81 +36,18 @@ public class Player extends Observable
 	
 	public double getPlayerLocationX() 
 	{
-		//System.out.println("x: " + currentLocation.x + " | y: " + currentLocation.y);
 		return playerRect.getX();
 	}
 	
 	public double getPlayerLocationY() 
 	{
-		//System.out.println("x: " + currentLocation.x + " | y: " + currentLocation.y);
 		return playerRect.getY();
 	}
-	
-	public void Timer(Pane pane)
-	{
-		playerTimer = new AnimationTimer() {
-            private long lastUpdate = -1 ;
-            @Override
-            public void handle(long now) {
-                long elapsedNanos = now - lastUpdate ;
-                if (lastUpdate < 0) {
-                    lastUpdate = now ;
-                    return ;
-                }
-                double elapsedSeconds = elapsedNanos / 1_000_000_000.0 ;
-                double deltaX = 0 ;
-                double deltaY = 0 ;
-                if (right) deltaX += speed ;
-                if (left) deltaX -= speed ;
-                if (down) deltaY += speed ;
-                if (up) deltaY -= speed ;
-                playerRect.setX(clampRange(playerRect.getX() + deltaX * elapsedSeconds, 0, pane.getWidth() - playerRect.getWidth()));
-                playerRect.setY(clampRange(playerRect.getY() + deltaY * elapsedSeconds, 0, pane.getHeight() - playerRect.getHeight()));
-                lastUpdate = now ;
-            }
-        };
+		
+	public void move() {
+		setChanged();
+		notifyObservers(this);
 	}
 	
-	 public void processKey(KeyCode code, boolean on) {
-	        switch (code) {
-	        case A:
-	    		setChanged();
-	    		notifyObservers(this);
-	            left = on ;
-	            break ;
-	        case D: 
-	    		setChanged();
-	    		notifyObservers(this);
-	            right = on ;
-	            break ;
-	        case W:
-	    		setChanged();
-	    		notifyObservers(this);
-	            up = on ;
-	            break ;
-	        case S:
-	    		setChanged();
-	    		notifyObservers(this);
-	            down = on ;
-	            break ;
-	            /*
-	        case UP: 
-	        	shoot(player);
-	        case LEFT:
-	        	shoot(player);
-	        case RIGHT: 
-	        	shoot(player);
-	        case DOWN:
-	        	shoot(player);
-	        	*/
-	        default:
-	            break ;
-	        }
-	    }
-	 
-	    private double clampRange(double value, double min, double max) {
-	        if (value < min) return min ;
-	        if (value > max) return max ;
-	        return value ;
-	    }
+
 }
